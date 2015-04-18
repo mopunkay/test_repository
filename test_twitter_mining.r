65a8a038a9d41b81d95e9e31945031c79b87e250
Marc <mopunkay@gmail.com>
2015-04-17 18:59:16 -0700

    add instructions reference and modify twitter test code

--- test_twitter_mining.r
+++ test_twitter_mining.r
@@ -1,3 +1,4 @@
+# load packages
 library(twitteR)
 library(httpuv)
 library(tm)
@@ -6,16 +7,16 @@ library(RWeka)
 library(RWekajars)
 library(rJava)
 
-setup_twitter_oauth(consumer_key = "n3WHdHx6dkyHRmCgG8Esf4pfG", 
-	consumer_secret = "8nchLgJlwuEl8LnfJCiamkwcTskt1EVqvYa1caHzDBqBHmvk4K")
+# authenticate twitter account
+setup_twitter_oauth(consumer_key = "", consumer_secret = "")
 
-
+# pull tweets and create corpus
 #example.tweets.search <- searchTwitter('@delta', n=200)
-
 nba.tweets <- userTimeline("nbastats", n = 200)
 tweets.df <- do.call("rbind", lapply(nba.tweets, as.data.frame))
 tweets.corpus <- Corpus(VectorSource(tweets.df$text))
 
+# clean up text in corpus for more efficient analysis
 tweets.corpus <- tm_map(tweets.corpus, content_transformer(tolower))
 tweets.corpus <- tm_map(tweets.corpus, removePunctuation)
 tweets.corpus <- tm_map(tweets.corpus, removeNumbers)
@@ -28,10 +29,13 @@ myStopWords <- setdiff(myStopWords, c("ball", "basketball"))
 tweets.corpus <- tm_map(tweets.corpus, removeWords, myStopWords)
 tweets.corpus <- tm_map(tweets.corpus, stripWhitespace)
 
+# stem corpus
 tweets.corpus.st <- tm_map(tweets.corpus, stemDocument)
 #tweets.corpus.st <- tm_map(tweets.corpus.st, stemCompletion, 
 #														dictionary = tweets.corpus)
 
+# create document-term matrix out of corpus
 tweets.dtm <- DocumentTermMatrix(tweets.corpus.st)
 
+# do some analysis
 findFreqTerms(tweets.dtm, lowfreq = 20)
